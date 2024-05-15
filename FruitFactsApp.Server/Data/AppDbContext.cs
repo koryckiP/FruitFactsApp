4e32b13d8bce8a926a5e717c809d8f3db78202c7
Patryk Korycki <korycki.patryk0303@gmail.com>
2024-05-15 20:38:52 +0200

    Add project files.

---
+++ FruitFactsApp.Server/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using FruitFactsApp.Library.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FruitFactsApp.Server.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Fruit> Fruits { get; set; }
+    }
+
+}
---
+++ FruitFactsApp.Server/Migrations/20240515183428_init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FruitFactsApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fruits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fruits", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fruits");
+        }
+    }
+}
